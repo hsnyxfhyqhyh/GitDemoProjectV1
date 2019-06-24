1489a1fb4cd07a73ea5849fd14fde7eb7dc3d5d3
yahu <yahu@secure.midatlantic.aaa.com>
2019-06-24 11:42:41 -0400

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+	public static void main(String[] args) {
+		
+		String hungry = "Yes"; 
+		
+		if (hungry=="Yes") {
+			System.out.println("eat somehting");
+		}
+		else {
+			System.out.println("Do work ....");
+		}
+	}
+}
